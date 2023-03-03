de6e7b8a9563ea4b4846eed7a5a18b9868648570
Jonshea Nutson <yuurieru@icloud.com>
2023-03-03 09:58:26 -0500

    updated server.js

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.get('/', (req, res) => res.json({ message: 'The Server is Running' }))
 app.use('/api', AppRouter)
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
