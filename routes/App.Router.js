4bbe14fe01280be80e7d4142328b0de460939646
jcurti2 <joshua.curtis24@gmail.com>
2023-03-03 09:47:01 -0800

    watchlist router set up

--- routes/App.Router.js
+++ routes/App.Router.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
-const StockRouter = require('./StockRouter')
-// const WatchlistRouter = require('./WatchlistRouter')
+// const StockRouter = require('./StockRouter')
+const WatchlistRouter = require('./WatchlistRouter')
 
 Router.use('/users', UserRouter)
-Router.use('/stock', StockRouter)
-// Router.use('/watchlist', WatchlistRouter)
+// Router.use('/stocks', StockRouter)
+Router.use('/watchlists', WatchlistRouter)
 
 module.exports = Router
