e13f3a16fc4d5845f9ac6f7b0de9ebe428e7733f
Jonshea Nutson <yuurieru@icloud.com>
2023-03-03 13:59:43 -0500

    Register and Login for user now works and hashes

--- migrations/20230303151038-create-user.js
+++ migrations/20230303151038-create-user.js
@@ -1,42 +1,43 @@
-'use strict';
+'use strict'
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      email: { 
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }},
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: new Date()
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: new Date()
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('users');
-  }
-};
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+                validate: {
+                    isEmail: true,
+                },
+            },
+            passwordDigest: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: new Date(),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+                defaultValue: new Date(),
+            },
+        })
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('users')
+    },
+}
--- routes/App.Router.js
+++ routes/App.Router.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
-const StockRouter = require('./StockRouter')
+// const StockRouter = require('./StockRouter')
 // const WatchlistRouter = require('./WatchlistRouter')
 
 Router.use('/users', UserRouter)
-Router.use('/stock', StockRouter)
+// Router.use('/stock', StockRouter)
 // Router.use('/watchlist', WatchlistRouter)
 
 module.exports = Router
